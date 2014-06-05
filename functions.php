03000f743f6648a9f6abd0234a7d13654a1a22cf
Ian Noyes <ian@12southmusic.com>
2014-06-04 20:48:25 -0500

    add catch_that_image

--- functions.php
+++ functions.php
@@ -1,4 +1,15 @@
 <?php
+//Custom Functions
+
+// Get URL of first image in a post
+function catch_that_image() {
+global $post, $posts;
+$first_img = '';
+ob_start();
+ob_end_clean();
+$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+$first_img = $matches [1] [0];
+
 /**
  * Roots includes
  */
