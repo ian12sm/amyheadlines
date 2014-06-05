98d56df9fb551071ee83954f3d2fb9817e4c881a
Ian Noyes <ian@12southmusic.com>
2014-06-04 20:34:29 -0500

    post thumbnail of blog post

--- templates/content.php
+++ templates/content.php
@@ -3,6 +3,9 @@
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
+  <div class="post-img">
+  	<?php the_post_thumbnail(thumbnail); ?> 
+  </div>
   <div class="entry-summary">
     <?php the_excerpt(); ?>
   </div>
