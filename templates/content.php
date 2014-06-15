826a46ae013039ff075c95aadb39e7db39be1023
Ian Noyes <ian@12southmusic.com>
2014-06-15 17:19:29 -0500

    add h4

--- templates/content.php
+++ templates/content.php
@@ -1,7 +1,7 @@
 <article <?php post_class(); ?>>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php the_tags(); ?>
+    <h4 class="tags"><?php the_tags(); ?></h4>
   </header>
   <div class="post-img">
   	<?php the_post_thumbnail(large); ?> 
