40d1e35f7586aa3a8767257b6d3a5360166e1c23
Ian Noyes <ian@12southmusic.com>
2014-06-15 16:52:49 -0500

    try this

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,4 +1,5 @@
 <?php while (have_posts()) : the_post(); ?>
+	<?php echo "loop runs on content-page.php"; ?>
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
