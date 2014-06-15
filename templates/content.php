9434485e182de11626e92287893f9e8382e791a3
Ian Noyes <ian@12southmusic.com>
2014-06-15 16:54:14 -0500

    large thumbnails

--- templates/content.php
+++ templates/content.php
@@ -3,7 +3,7 @@
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
   </header>
   <div class="post-img">
-  	<?php the_post_thumbnail(thumbnail); ?> 
+  	<?php the_post_thumbnail(large); ?> 
   </div>
   <div class="entry-summary">
     <?php the_excerpt(); ?>
