7f6b93b4a0dc050ed009e2635d03f13c82bbdb71
Ian Noyes <ian@12southmusic.com>
2014-06-15 17:31:31 -0500

    echo categories

--- templates/content.php
+++ templates/content.php
@@ -1,7 +1,7 @@
 <article <?php post_class(); ?>>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <h4 class="tags"><?php get_the_category_list(); echo "function is here"; ?></h4>
+    <h4 class="tags"><?php $categories = get_the_category_list(); echo $categories; ?></h4>
   </header>
   <div class="post-img">
   	<?php the_post_thumbnail(large); ?> 
