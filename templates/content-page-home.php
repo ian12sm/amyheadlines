b7ea7ea5160d721050b78b411d25ba29000741ac
Ian Noyes <ian@12southmusic.com>
2014-06-15 16:50:38 -0500

    fix my lousy syntax

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,5 +1,5 @@
 <?php while (have_posts()) : the_post(); ?>
-	<?php echo "one loop";
+	<?php echo "one loop";?>
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
