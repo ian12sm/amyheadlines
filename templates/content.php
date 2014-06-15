f5b425b9aed96a053ceb90f0dd89689a4fa8a259
Ian Noyes <ian@12southmusic.com>
2014-06-15 17:28:59 -0500

    categories not tags

--- templates/content.php
+++ templates/content.php
@@ -1,13 +1,7 @@
 <article <?php post_class(); ?>>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <h4 class="tags"><?php $posttags = get_the_tags(); 
-    if ($posttags) {
-  foreach($posttags as $tag) {
-    echo $tag->name . ' '; 
-  }
-	}?>
-</h4>
+    <h4 class="tags"><?php get_the_category_list(); ?></h4>
   </header>
   <div class="post-img">
   	<?php the_post_thumbnail(large); ?> 
