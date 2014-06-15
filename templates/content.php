09af94811bbf0451aaa122458b06a307ca9be699
Ian Noyes <ian@12southmusic.com>
2014-06-15 17:23:15 -0500

    get_the_tags

--- templates/content.php
+++ templates/content.php
@@ -1,7 +1,13 @@
 <article <?php post_class(); ?>>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <h4 class="tags"><?php the_tags(); ?></h4>
+    <h4 class="tags"><?php $posttags = get_the_tags(); 
+    if ($posttags) {
+  foreach($posttags as $tag) {
+    echo $tag->name . ' '; 
+  }
+	}?>
+</h4>
   </header>
   <div class="post-img">
   	<?php the_post_thumbnail(large); ?> 
