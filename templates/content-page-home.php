d62f95a292b943a154b12a00a6186d6c4d0f1fc4
Ian Noyes <ian@12southmusic.com>
2014-06-15 16:41:29 -0500

    loop checking

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,4 +1,5 @@
 <?php while (have_posts()) : the_post(); ?>
+	<?php echo "one loop";
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
