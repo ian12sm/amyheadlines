a8781c449d2244d123ad08edf70ac2d55630fe7e
Ian Noyes <ian@12southmusic.com>
2014-06-15 17:10:03 -0500

    get post terms

--- templates/content.php
+++ templates/content.php
@@ -1,6 +1,7 @@
 <article <?php post_class(); ?>>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <?php $terms = wp_get_post_terms( $post_id, $taxonomy, $args ); echo $terms; ?>
   </header>
   <div class="post-img">
   	<?php the_post_thumbnail(large); ?> 
