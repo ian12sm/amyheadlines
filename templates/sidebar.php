a2ec252b6180690499ce20a25471830b7c8075fe
Ian Noyes <ian@12southmusic.com>
2014-06-22 11:10:18 -0500

    display five posts on the sidebar

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,3 +1,18 @@
 <?php dynamic_sidebar('sidebar-primary'); ?>
-<!--SIDEBEAR-->
-!!!!!!!!!!!!!!!!!!!!!!!!!
+<?php 
+	$args = array(
+	'posts_per_page'   => 5,
+	'offset'           => 0,
+	'category'         => '',
+	'orderby'          => 'post_date',
+	'order'            => 'DESC',
+	'include'          => '',
+	'exclude'          => '',
+	'meta_key'         => '',
+	'meta_value'       => '',
+	'post_type'        => 'post',
+	'post_mime_type'   => '',
+	'post_parent'      => '',
+	'post_status'      => 'publish',
+	'suppress_filters' => true );
+	$posts_array = get_posts( $args ); ?>
