2ca34a39943050d52d168926e2735424bef218d0
Ian Noyes <ian@12southmusic.com>
2014-06-21 20:55:19 -0500

    is single sidebar

--- lib/config.php
+++ lib/config.php
@@ -55,7 +55,7 @@ function roots_display_sidebar() {
      */
     array(
       'is_404',
-      'is_front_page'
+      'is_singular'
     ),
     /**
      * Page template checks (via is_page_template())
