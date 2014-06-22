131454894c0ecfe08487f4bdc8c5b2b10496b313
Ian Noyes <ian@12southmusic.com>
2014-06-22 11:19:28 -0500

    foreach cat

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -3,26 +3,29 @@
 <?php 
 	$post_id = get_the_ID();
 	$category = get_the_category($post_id);
-	echo "<h3>" . $category[0]->cat_name ."</h3>";
-	$args = array(
-	'posts_per_page'   => 5,
-	'offset'           => 0,
-	'category'         => '$category',
-	'orderby'          => 'post_date',
-	'order'            => 'DESC',
-	'include'          => '',
-	'exclude'          => '',
-	'meta_key'         => '',
-	'meta_value'       => '',
-	'post_type'        => 'post',
-	'post_mime_type'   => '',
-	'post_parent'      => '',
-	'post_status'      => 'publish',
-	'suppress_filters' => true );
-	$posts_array = get_posts( $args ); 
-	foreach ( $posts_array as $post ) : setup_postdata( $post ); ?>
-	<li>
-		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-	</li>
-<?php endforeach; 
-wp_reset_postdata();?>
+	foreach((get_the_category()) as $category) { 
+		echo "<h3>" . $category[0]->cat_name ."</h3>";
+		$args = array(
+		'posts_per_page'   => 5,
+		'offset'           => 0,
+		'category'         => '$category',
+		'orderby'          => 'post_date',
+		'order'            => 'DESC',
+		'include'          => '',
+		'exclude'          => '',
+		'meta_key'         => '',
+		'meta_value'       => '',
+		'post_type'        => 'post',
+		'post_mime_type'   => '',
+		'post_parent'      => '',
+		'post_status'      => 'publish',
+		'suppress_filters' => true );
+		$posts_array = get_posts( $args ); 
+		foreach ( $posts_array as $post ) : setup_postdata( $post ); ?>
+		<li>
+			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		</li>
+	<?php endforeach; 
+	wp_reset_postdata();
+		endforeach;
+	?>
