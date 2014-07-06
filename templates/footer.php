e9b872e8736326c3d45e523d400c33dc342681ec
Ian Noyes <ian@12southmusic.com>
2014-07-06 15:49:12 -0500

    Add social nav

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,26 @@
-<footer class="content-info" role="contentinfo">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+<footer class="content-info container-fluid" role="contentinfo">
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="row sidebar-footer">
+      	<?php dynamic_sidebar('sidebar-footer'); ?>
+    </div>
+    <div class="row footer-nav">  
+        <?php
+     if (has_nav_menu('footer_navigation')) :
+       wp_nav_menu(array('theme_location' => 'footer_navigation', 'menu_class' => ''));
+     endif;
+  ?>
+    </div>
+         
+      <div id="social" class="row social-nav">
+  <?php
+     if (has_nav_menu('social_navigation')) :
+       wp_nav_menu(array('theme_location' => 'social_navigation', 'menu_class' => ''));
+     endif;
+  ?>
+  </div>
+      <p class="credits">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>  |  <a href="https://twitter.com/I_noyes" target="_blank">built by Ian Noyes</a></p>
+    </div>
   </div>
 </footer>
 
