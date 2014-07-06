3f14baeb34490e9b24321c27c95fd9aae2359e1f
Ian Noyes <ian@12southmusic.com>
2014-07-06 16:34:41 -0500

    back outside

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -17,10 +17,10 @@
           wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
         endif;
       ?>
-      <form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
+    </nav>
+    <form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
 <div><input type="text" size="put_a_size_here" name="s" id="s" value="Write your search and hit Enter" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/>
 <input type="submit" id="searchsubmit" value="Search" class="btn" />
-    </nav>
 </div>
 </form>
   </div>
