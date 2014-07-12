ffb4a3c1fa40f413390a2a38e8394164005575fc
Ian Noyes <ian@12southmusic.com>
2014-07-11 21:36:08 -0500

    #social-may

--- templates/content-page-maintenance.php
+++ templates/content-page-maintenance.php
@@ -1,6 +1,6 @@
 <h1>New website coming soon!</h1>
 <h3>In the meantime reach out to us on social media!</h3>
-<div id='social'>
+<div id='social-may'>
 	<ul class='socials'>
 		<li class="menu-"><a href="https://www.facebook.com/AmyHeadCosmetics"><i class='fa fa-facebook'></i></a></li>
 		<li class="menu-"><a href="https://twitter.com/AmyHeadHomewood"><i class='fa fa-twitter'></i></a></li>
