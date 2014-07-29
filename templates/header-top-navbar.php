ca1ed001a228d7a7c43d07648af0015321c2ee00
Ian Noyes <ian@12southmusic.com>
2014-07-28 22:17:01 -0500

    data-parent

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -12,7 +12,7 @@
       <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
     </div>
 
-    <nav class="collapse navbar-collapse" id='nav-collapse' role="navigation">
+    <nav class="collapse navbar-collapse" id='nav-collapse' data-parent='.navbar-toggle' role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
           wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
