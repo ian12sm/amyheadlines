979c3ef9af53b445fe955e20fc02f1928c124ee1
Ian Noyes <ian@12southmusic.com>
2014-07-28 21:34:07 -0500

    brioso pro

--- templates/head.php
+++ templates/head.php
@@ -8,6 +8,7 @@
   <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700,300italic' rel='stylesheet' type='text/css'>
   <?php wp_head(); ?>
-
+  <script type="text/javascript" src="//use.typekit.net/scb0koj.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 </head>
