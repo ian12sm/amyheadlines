6c7735812c16d7a80af56dd40692f01c857b8782
Ian Noyes <ian@12southmusic.com>
2014-07-30 21:25:18 -0500

    i9mg responsive

--- lib/custom.php
+++ lib/custom.php
@@ -2,6 +2,13 @@
 /**
  * Custom functions
  */
+add_filter('post_thumbnail_html','add_class_to_thumbnail');
+function add_class_to_thumbnail($thumb) {
+if( is_single() ) $thumb = str_replace('attachment-', 'img-responsive attachment-', $thumb);
+return $thumb;
+}
+
+
 function custom_scripts() {
   wp_enqueue_style('roots_custom', get_template_directory_uri() . '/assets/css/app.css');
 }
