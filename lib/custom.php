04b5df89781ce40432bacf5f12618745d61e7c05
Ian Noyes <ian@12southmusic.com>
2014-07-30 21:26:20 -0500

    just do this

--- lib/custom.php
+++ lib/custom.php
@@ -2,11 +2,6 @@
 /**
  * Custom functions
  */
-add_filter('post_thumbnail_html','add_class_to_thumbnail');
-function add_class_to_thumbnail($thumb) {
-if( is_single() ) $thumb = str_replace('attachment-', 'img-responsive attachment-', $thumb);
-return $thumb;
-}
 
 
 function custom_scripts() {
