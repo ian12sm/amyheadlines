f9985f77a57d8b537eb885bcc71b6b0eb85cf1f0
Ian Noyes <ian@12southmusic.com>
2014-08-02 10:36:59 -0500

    remove the colon

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,6 +1,6 @@
 <?php dynamic_sidebar('sidebar-primary'); ?>
 <h2>Related Posts</h2>
-<?php 
+<?php
 	$post_id = get_the_ID();
 	$category = get_the_category($post_id);
 	foreach((get_the_category()) as $category){
@@ -20,12 +20,12 @@
 		'post_parent'      => '',
 		'post_status'      => 'publish',
 		'suppress_filters' => true );
-		$posts_array = get_posts( $args ); 
-		foreach ( $posts_array as $post ){ : setup_postdata( $post );  ?>
+		$posts_array = get_posts( $args );
+		foreach ( $posts_array as $post ){ setup_postdata( $post );  ?>
 		<li>
 			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 		</li>
 	<?php }
-	} 
+	}
 	wp_reset_postdata();
 	?>
