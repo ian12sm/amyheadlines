372ac7737f031f3dcac0b2b470d26d9e14d4adb3
Ian Noyes <ian@12southmusic.com>
2014-08-17 13:55:56 -0500

    analytics

--- templates/footer.php
+++ templates/footer.php
@@ -4,14 +4,14 @@
       <div class="row sidebar-footer">
       	<?php dynamic_sidebar('sidebar-footer'); ?>
     </div>
-    <div class="row footer-nav">  
+    <div class="row footer-nav">
         <?php
      if (has_nav_menu('footer_navigation')) :
        wp_nav_menu(array('theme_location' => 'footer_navigation', 'menu_class' => ''));
      endif;
   ?>
     </div>
-         
+
       <div id="social" class="row social-nav">
   <?php
      if (has_nav_menu('social_navigation')) :
@@ -22,6 +22,17 @@
       <p class="credits">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>  |  <a href="https://twitter.com/I_noyes" target="_blank">built by Ian Noyes</a></p>
     </div>
   </div>
+  <!--Begin Analytics-->
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-53916002-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </footer>
 
 <?php wp_footer(); ?>
