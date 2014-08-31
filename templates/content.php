640ea7b4209f85133e5353820d32018e2307a38e
Ian Noyes <ian@12southmusic.com>
2014-08-31 12:18:59 -0500

    comments

--- templates/content.php
+++ templates/content.php
@@ -1,6 +1,7 @@
 <article <?php post_class(); ?>>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <h3><a href="<?php the_permalink(); ?>"></a></h3>
     <h4 class="tags">
     <?php $categories = get_the_category_list();
     echo $categories;
@@ -13,6 +14,7 @@
   <div class="entry-summary">
   <?php if( $wp_query->current_post == 0 && !is_paged() ) {
   	the_content();
+    comments_template('/templates/comments.php');
   }
   else{
 	the_excerpt();
