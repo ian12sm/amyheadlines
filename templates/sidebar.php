8aada24b4ce49546370949c6dd5a1eec403200b7
Ian Noyes <ian@12southmusic.com>
2014-09-02 22:26:31 -0500

    signup in the sidebar

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -30,3 +30,21 @@
 	}
 	wp_reset_postdata();
 	?>
+	<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="//amyheadlines.us9.list-manage.com/subscribe/post?u=9058e8f1491ecc5e38882a801&amp;id=f24cbd857a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+	<h3>Subscribe to our mailing list for more Amyheadlines updates!</h3>
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;"><input type="text" name="b_9058e8f1491ecc5e38882a801_f24cbd857a" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
