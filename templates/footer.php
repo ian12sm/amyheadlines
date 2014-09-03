45c29e5e9fe73c488ccd50b6b357e6ab3cfd90d4
Ian Noyes <ian@12southmusic.com>
2014-09-02 21:53:13 -0500

    clear both

--- templates/footer.php
+++ templates/footer.php
@@ -26,15 +26,17 @@
   /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
      We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
 </style>
-<div id="mc_embed_signup">
-<form action="//amyheadlines.us9.list-manage.com/subscribe/post?u=9058e8f1491ecc5e38882a801&amp;id=f24cbd857a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-  <label for="mce-EMAIL">Subscribe to our mailing list</label>
-  <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
-    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;"><input type="text" name="b_9058e8f1491ecc5e38882a801_f24cbd857a" tabindex="-1" value=""></div>
 
-</form>
+    <div id="mc_embed_signup">
+    <form action="//amyheadlines.us9.list-manage.com/subscribe/post?u=9058e8f1491ecc5e38882a801&amp;id=f24cbd857a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+      <label for="mce-EMAIL">Subscribe to our mailing list</label>
+      <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px;"><input type="text" name="b_9058e8f1491ecc5e38882a801_f24cbd857a" tabindex="-1" value=""></div>
+    </form>
+    </div>
+  </div>
 </div>
 
 <!--End mc_embed_signup-->
