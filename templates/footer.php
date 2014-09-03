681dd37145d75a6cb43d1f553a16f9bb802c3776
Ian Noyes <ian@12southmusic.com>
2014-09-02 22:03:00 -0500

    change tagline

--- templates/footer.php
+++ templates/footer.php
@@ -29,7 +29,7 @@
 
     <div id="mc_embed_signup">
     <form action="//amyheadlines.us9.list-manage.com/subscribe/post?u=9058e8f1491ecc5e38882a801&amp;id=f24cbd857a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-      <h3>Subscribe to our mailing list</h3>
+      <h3>Subscribe to our mailing list for more Amyheadlines updates!</h3>
       <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
         <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
         <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
