ec210f8ee2c9a33191a793d126cfb43baaa8dd6f
ian12sm <ian@12southmusic.com>
2015-01-15 21:15:14 -0600

    this?

--- templates/content-page-test.php
+++ templates/content-page-test.php
@@ -7,8 +7,8 @@
               'post_status' => 'any',
               'posts_per_page' => 10,
             );
-    $post_query = new WP_Query($args);
+    $posts_array = new WP_Query($args);
 
-	while (have_posts()) : the_post(); ?>
+	while ($posts_array->have_posts()) : $posts_array->the_post(); ?>
 	<h1><?php the_title(); ?></h1>
  <?php endwhile; ?>
