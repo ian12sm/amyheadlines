111492b09f1f91a41993ad191cf16c3a74dfd6f0
ian12sm <ian@12southmusic.com>
2015-01-15 21:02:17 -0600

    inspiration

--- templates/content-page-test.php
+++ templates/content-page-test.php
@@ -2,10 +2,11 @@
 <?php 
 	$args = array(
         'post_type' => 'post'
+        'category_slug' => 'inspiration'
     );
 
     $post_query = new WP_Query($args);
 
 	while (have_posts()) : the_post(); ?>
-	<h1 class="h1"><?php the_title(); ?></h1>
+	<h1><?php the_title(); ?></h1>
  <?php endwhile; ?>
