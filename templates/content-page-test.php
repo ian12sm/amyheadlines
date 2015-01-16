09c48815a6cba0ceaf428f6d3e87fb664606e824
ian12sm <ian@12southmusic.com>
2015-01-15 21:04:39 -0600

    query

--- templates/content-page-test.php
+++ templates/content-page-test.php
@@ -1,6 +1,6 @@
 <h1>TEST</h1>
 <?php 
-    $post_query = new WP_Query('category_name=inspiration');
+    $post_query = new WP_Query();
 
 	while (have_posts()) : the_post(); ?>
 	<h1><?php the_title(); ?></h1>
