f5d85604ce252e2b6062e623efa6e8894fe4cf81
ian12sm <ian@12southmusic.com>
2015-01-15 21:08:28 -0600

    try this!

--- templates/content-page-test.php
+++ templates/content-page-test.php
@@ -1,6 +1,13 @@
 <h1>TEST</h1>
 <?php 
-    $post_query = new WP_Query();
+    $args = array(
+              'post_type' => 'post',
+              'orderby'   => 'title',
+              'order'     => 'ASC',
+              'post_status' => 'any',
+              'posts_per_page' => 10,
+            );
+    $post_query = new WP_Query($args);
 
 	while (have_posts()) : the_post(); ?>
 	<h1><?php the_title(); ?></h1>
