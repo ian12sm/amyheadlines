24cf7159f6c31607a2098f410dee6a9c68bfe1bf
ian12sm <ian@12southmusic.com>
2015-01-15 21:34:25 -0600

    classes

--- templates/content-page-test.php
+++ templates/content-page-test.php
@@ -1,14 +1,19 @@
-<h1>TEST</h1>
-<?php 
-    $args = array(
-              'post_type' => 'post',
-              'orderby'   => 'title',
-              'order'     => 'ASC',
-              'post_status' => 'any',
-              'posts_per_page' => 10,
-            );
-    $posts_array = new WP_Query($args);
+<div class="row">
+	<?php 
+	    $args = array(
+	              'post_type' => 'post',
+	              'orderby'   => 'title',
+	              'order'     => 'ASC',
+	              'post_status' => 'any',
+	              'posts_per_page' => 10,
+	            );
+	    $posts_array = new WP_Query($args);
 
 	while ($posts_array->have_posts()) : $posts_array->the_post(); ?>
-	<h1><?php the_title(); ?></h1>
- <?php endwhile; ?>
+		<div class="col-sm-3">
+			<div class="img-lq thumb-card">
+			<?php the_post_thumbnail('medium'); ?> 
+			</div>
+		</div>
+	<?php endwhile; ?>
+</div>
