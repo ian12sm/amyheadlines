c95da37c3dc59f8350526e3c8e50832e460c5c1f
ian12sm <ian@12southmusic.com>
2015-01-15 20:51:33 -0600

    query

--- templates/content-page-test.php
+++ templates/content-page-test.php
@@ -1,3 +1,10 @@
-<?php while (have_posts()) : the_post(); ?>
-	<h1><?php the_title(); ?>
+<?php 
+	$args = array(
+        'post_type' => 'post'
+    );
+
+    $post_query = new WP_Query($args);
+
+	while (have_posts()) : the_post(); ?>
+	<h1><?php the_title(); ?></h1>
  <?php endwhile; ?>
