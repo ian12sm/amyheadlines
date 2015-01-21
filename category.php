135b1befcd8ea0a87b85cef4e639e4290690ff47
ian12sm <ian@12southmusic.com>
2015-01-20 21:22:32 -0600

    implement new cat styles

--- category.php
+++ category.php
@@ -1,5 +1,17 @@
-<?php while (have_posts()) : the_post(); ?>
-	<span class='hidden'>TEST HERE</span>
-  <?php the_content(); ?>
-  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
-<?php endwhile; ?>
+<div class="row cat-img-grid">
+	<?php
+	while (have_posts()) : the_post(); ?>
+		<div class="col-lg-3 col-sm-4">
+			<a href="<?php the_permalink(); ?>">
+				<div class="card-container">
+					<div class="img-lq thumb-card">
+					<?php the_post_thumbnail('medium'); ?> 
+					</div>
+					<div class="thumb-overlay">
+						<h1><?php the_title(); ?></h1>
+					</div>
+				</div>
+			</a>
+		</div>
+	<?php endwhile; ?>
+</div>
